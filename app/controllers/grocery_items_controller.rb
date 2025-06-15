bb12565fea86af47c2f2ea58ae76a647e254262f
Mo <mouhamadou.diouf.1@gmail.com>
2025-06-15 13:53:02 +0000

    update groceryitems controller

--- app/controllers/grocery_items_controller.rb
+++ app/controllers/grocery_items_controller.rb
@@ -1,22 +1,49 @@
 class GroceryItemsController < ApplicationController
+  before_action :set_grocery_item, only: [ :show, :edit, :update, :destroy ]
+
   def index
+    @grocery_items = GroceryItem.all.order(created_at: :desc)
   end
 
   def show
   end
 
   def new
+    @grocery_item = GroceryItem.new
   end
 
   def create
+    @grocery_item = GroceryItem.new(grocery_item_params)
+    if @grocery_item.save
+      redirect_to @grocery_item, notice: "Grocery item was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @grocery_item.update(grocery_item_params)
+      redirect_to @grocery_item, notice: "Grocery item was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @grocery_item.destroy
+    redirect_to grocery_items_path, notice: "Grocery item was successfully deleted."
+  end
+
+  private
+
+  def set_grocery_item
+    @grocery_item = GroceryItem.find(params[:id])
+  end
+
+  def grocery_item_params
+    params.require(:grocery_item).permit(:name, :price, :category, :image_url, :description)
   end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get home page" do
+    get root_url
+    assert_response true
+  end
 end
